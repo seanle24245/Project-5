f26a158b64c4c42dabc06a41bdef96ce2976e60e
seanle24245 <sean.le24245@gmail.com>
2019-11-17 17:01:04 -0500

    ok
    try again

--- src/DataSolver.java
+++ src/DataSolver.java
@@ -1,7 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class DataSolver {
     private Scanner songScanner;
     private Scanner dataScanner;
+    
+    
+    public DataSolver(String songFile, String dataFile) throws FileNotFoundException {
+        songScanner = new Scanner(new File(songFile));
+        dataScanner = new Scanner(new File(dataFile));
+        
+        
+    }
     
 }
