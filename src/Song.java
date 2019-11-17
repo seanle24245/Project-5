6f6ebeb1137725a97652e4e2da56b9176f464427
seanle24245 <sean.le24245@gmail.com>
2019-11-17 16:46:04 -0500

    song class complete
    song class done i think

--- src/Song.java
+++ src/Song.java
@@ -5,12 +5,59 @@ public class Song {
     private int year;
     private int amountOfYes;
     private int totalResponses;
-    
+    private int[] hobbyData;
+    private int[] regionData;
+    private int[] majorData;
+    public static final int ARRAY_SIZE = 16;
     public Song(String title, String genre, int year) {
         this.title = title;
         this.genre = genre;
         this.year = year;
-        amountOfYes = 0;
-        totalResponses = 0;
+        this.amountOfYes = 0;
+        this.totalResponses = 0;
+        hobbyData = new int[ARRAY_SIZE];
+        regionData = new int[ARRAY_SIZE];
+        majorData = new int[ARRAY_SIZE];
+        
     }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getGenre() {
+        return genre;
+    }
+    
+    public int getYear() {
+        return year;
+    }
+    
+    public double calcPercentage() {
+        return (double)(amountOfYes/totalResponses);
+    }
+    
+    public int getHobbyData(int x) {
+        return hobbyData[x];
+    }
+    
+    public int getRegionData(int x) {
+        return regionData[x];
+    }
+    
+    public int getMajorData(int x) {
+        return majorData[x];
+    }
+    
+    public void incrementYes() {
+        amountOfYes++;
+        totalResponses++;
+    }
+    
+    public void incrementNo() {
+        totalResponses++;
+    }
+    
+    
+    
 }
